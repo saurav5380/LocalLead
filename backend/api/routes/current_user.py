7f586ba4c4f50d478355101429f6dc22a08e603c
saurav5380 <saurav5380@gmail.com>
2025-12-25 17:49:29 +0530

    error checking and updates to data returned for logged in user

--- backend/api/routes/current_user.py
+++ backend/api/routes/current_user.py
@@ -5,13 +5,12 @@ from services.get_current_user import get_current_user
 router = APIRouter()
 
 
-@router.get("/currentuser",status_code=status.HTTP_302_FOUND)
-async def user_details(user = Depends(get_current_user)):
-    try:
-        if user:
-            return {"message": f"You are logged in as user: {user}"}
-    except Exception as e:
-        return {"error": f"Could not fetch user details due to error: {str(e)}"}
+@router.get("/currentuser",status_code=status.HTTP_200_OK)
+async def user_details(user: dict = Depends(get_current_user)):
+    return {"message": f"You are logged in as user: {user.username}",
+            "full_name": f"Your full name is: {user.full_name}",
+            "email": f"Your email is: {user.email}"
+            }
     
 
         
--- backend/services/get_current_user.py
+++ backend/services/get_current_user.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
 from typing import Optional
-from jose import jwt, JWTError
-
+from jose import jwt, JWTError, ExpiredSignatureError
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from db import get_db
 
 load_dotenv()
 
@@ -16,18 +18,34 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 class TokenPayload(BaseModel):
     sub: Optional[str] = None
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> TokenPayload:
+async def get_current_user(token: str = Depends(oauth2_scheme),
+                           db: Session = Depends(get_db)) -> dict:
+    
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorised access")
-        token_data = TokenPayload(sub=username)
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                detail="Invalid token: missing user information")
+        response = db.execute(text("SELECT username, email, full_name FROM users WHERE username = :username", {"username": username }))
+        user = response.fetchone()
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="Invalid Username, account does not exist.")
+        
+    
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Token has expired")
 
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorised access")
     
-    return token_data
+    return {
+        "username": user[0],
+        "email": user[1],
+        "full_name": user[2]
+    }
 
 
 
