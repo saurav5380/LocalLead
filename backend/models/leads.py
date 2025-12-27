385cb9fe0bab89ae29dfcbacf57e3a10d9efe016
saurav5380 <saurav5380@gmail.com>
2025-12-27 23:00:44 +0530

    leads init

---
+++ backend/models/leads.py
@@ -0,0 +1,29 @@
+from db import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import (String,DateTime, Integer, Enum, func, ForeignKey)
+from backend.models.users import User
+import enum
+from datetime import datetime
+
+class StatusEnum(enum.Enum):
+    new = "new"
+    contacted = "contacted"
+    qualified = "qualified"
+    won = "won"
+    lost = "lost"
+
+
+
+class Leads(Base):
+    __tablename__ = "leads"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    phone: Mapped[str] = mapped_column(String, nullable=True)
+    company_name: Mapped[str] = mapped_column(String, nullable=True)
+    status: Mapped[StatusEnum] = mapped_column(Enum(StatusEnum), nullable=False, server_default="new")
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+    user: Mapped[User] = relationship(back_populates="leads")
+
--- backend/models/users.py
+++ backend/models/users.py
@@ -1,7 +1,11 @@
 from db import Base
 from sqlalchemy.orm import Mapped, relationship, mapped_column
-from sqlalchemy import (String, DateTime, Integer, Index) 
+from sqlalchemy import (String, DateTime, Integer) 
 from sqlalchemy.sql import func
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING: 
+    from leads import Leads
 
 class User(Base):
     __tablename__ = "users"
@@ -11,7 +15,7 @@ class User(Base):
     password_hash: Mapped[String] = mapped_column(String, nullable=False)
     full_name: Mapped[String] = mapped_column(String, nullable=False)
     created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
-    
+    leads: Mapped[List["Leads"]] = relationship(back_populates="user")
 
 
 
