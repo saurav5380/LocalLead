ceaa86a9f638446e3b935b0be98c8eca3bd75771
saurav5380 <saurav5380@gmail.com>
2026-01-08 20:27:38 +0530

    updated lead deletion

--- backend/api/routes/delete_lead.py
+++ backend/api/routes/delete_lead.py
@@ -4,9 +4,18 @@ from sqlalchemy import text
 from services.get_current_user import get_current_user
 from backend.db import get_db
 
-
 router = APIRouter()
 
-@router.delete("deletelead/{id}",status_code=status.HTTP_204_NO_CONTENT)
-async def delete_lead(id:int, db:Session = Depends(get_db), current_user = Depends(get_current_user)):
-    
+@router.delete("/deletelead/{lead_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_lead(lead_id:int, db:Session = Depends(get_db), current_user = Depends(get_current_user)):
+    try:
+        search_lead_query = text("SELECT * FROM leads WHERE id=:lead_id AND user_id=:userid")
+        lead_found = db.execute(search_lead_query, {"lead_id" : lead_id, "userid": current_user["user_id"]}).fetchone()
+        if lead_found:
+            result = db.execute(text("DELETE FROM leads WHERE id=:lead_id AND user_id=:userid RETURNING *"), {"lead_id": lead_id, "userid": current_user["user_id"]}).fetchone()
+            if result:
+                db.commit()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Lead with {lead_id} not found. Error {str(e)}") 
+       
